f0bf51d28b25452da8186266f995515a33afde39
ggrunwald97 <43156644+ggrunwald97@users.noreply.github.com>
2018-09-11 18:48:20 -0400

    change RawPC to XenVM, use CENTOS7, etc.

--- profile.py
+++ profile.py
@@ -16,9 +16,10 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.RawPC("node")
+
+node = request.XenVM("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node.routable_control_ip = "true"
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
